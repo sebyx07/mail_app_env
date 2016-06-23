e88054db1271177b3a0e125a75dccb1e795ec753
sebi <gore.sebyx@yahoo.com>
2016-06-23 20:24:36 +0300

    add the the transition to mailbox

--- frontend/app/components/login/login-form.js
+++ frontend/app/components/login/login-form.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
-  router: Ember.inject.service(),
+  router: Ember.inject.service('-routing'),
 
   tagName: 'form',
   classNames: ['col s12', 'login--login-form'],
@@ -16,6 +16,7 @@ export default Ember.Component.extend({
     Ember.$.post('/users/login', {email: email, password: password})
       .done((data) => {
         this.get('store').pushPayload(data);
+        this.get('router').transitionTo('mailbox');
       });
   }
 });
