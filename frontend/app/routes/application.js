b55b7010f3f7c30a64ee10c98c765cd375e00bb1
sebi <gore.sebyx@yahoo.com>
2016-06-23 20:02:59 +0300

    add the loginf orm

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -2,8 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    Ember.$.getJSON('/users/current_user').then((data) => {
+    Ember.$.getJSON('/users/current_user')
+    .then((data) => {
       this.store.pushPayload(data);
+    })
+    .fail(() => {
+      this.transitionTo('login');
     });
   }
 });
